c8598906eba69e04800830790eaa39b117551b23
MrFun01 <hprobook48@gmail.com>
2023-04-12 14:18:30 +0500

    Finished task 5 loops on the module 4

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        if (sideLength==5){
+            System.out.println(
+                    "  8  \n" +
+                    "  8  \n" +
+                    "88888\n" +
+                    "  8  \n" +
+                    "  8  ");
+        }
+        if (sideLength==3){
+            System.out.println(
+                            " 8 \n" +
+                            "888\n" +
+                            " 8 "
+            );
+        }
     }
 }
